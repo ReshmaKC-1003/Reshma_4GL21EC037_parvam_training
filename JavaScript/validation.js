977a1d4650ab694eaeb541f5c4700fdc737f6167
ReshmaKC-1003 <reshmakc555@gmail.com>
2025-03-18 21:40:38 +0530

    javascript concepts

---
+++ JavaScript/validation.js
@@ -0,0 +1,15 @@
+const form = document. getElementById("registration_form");
+
+
+function validateForm(event){
+    event.preventDefault();
+
+const  studentName = document.getElementById("student_name");
+const nameError = document.getElementsByClassName("name_error");
+
+
+if(studentName.value == ''){
+studentName.style.border = "2px solid red";
+nameError[0].textContent = "Name cannot be empty!";
+}
+}
