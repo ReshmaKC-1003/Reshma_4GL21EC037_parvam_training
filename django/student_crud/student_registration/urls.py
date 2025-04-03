fa0b623d380a6231bcbb8d546f5ac459d95dbe3d
ReshmaKC-1003 <reshmakc555@gmail.com>
2025-04-03 12:39:30 +0530

    Django Concept

--- django/student_crud/student_registration/urls.py
+++ django/student_crud/student_registration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.registration_list, name='list'),
     path('create/', views.registration_form, name='form'),
-    path('view/', views.view_registration, name='view'),
-    path('delete/', views.delete_registration, name='delete'),
+    path('<int:pk>/', views.view_registration, name='view'),
+    path('<int:pk>/edit/',views.update_registration, name='update'),
+    path('<int:pk>/delete/', views.delete_registration, name='delete'),
 ]
